a30349b1214bab687f650fdb9684b77e5eeefe10
deenkumar11 <deenkumar11@gmail.com>
2022-07-24 16:46:20 +0530

    ...

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log ("hello")
+// const numbers = [1,2,3]
+
+// let [Deen,Bobby,Gopal] = numbers
+
+// console.log (Deen,Bobby,Gopal)
+
+// const fullStack = [
+//     ['html','css','js'],
+//     ['node','mongodb']
+// ]
+
+// const [frontEnd,backEnd]= fullStack
+
+// console.log(backEnd)
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let [num1, num2, num3, ...rest] = nums
+
+// console.log(rest)
+
+// Classes
+
+// Rectangle = class Rectangle2 {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }}
+    
+// console.log (Rectangle.name)
